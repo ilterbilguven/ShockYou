d914cf967ff6c22dbb3295916532a8b60b3e811c
Buğra Güler <hbguler@gmail.com>
2018-01-26 21:23:47 +0300

    New panels added.

---
+++ Assets/Scripts/Controllers/PanelController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PanelName
+{
+    MainMenuPanel = 1,
+    SelectLevelPanel = 2
+}
+
+public class PanelController : MonoBehaviour {
+
+    public MainMenuPanel MainMenuPanel;
+    public SelectLevelPanel SelectLevelPanel;
+
+    public GameObject ActivePanel;
+
+
+    public void OpenPanel(ContentType panelName)
+    {
+        //if(ActivePanel)
+    }
+}
---
+++ Assets/Scripts/Views/MainMenuPanel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuPanel : MonoBehaviour {
+
+    public void OnSelectLevelButtonClick()
+    {
+
+    }
+
+    public void OnOptionsButtonClick()
+    {
+
+    }
+}
