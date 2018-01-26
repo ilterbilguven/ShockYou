d4c209d267bf2001f41f95f39b8729994b29ad85
EmirhanKocak <emirhanberkayk@gmail.com>
2018-01-26 20:15:19 +0300

    GameController Commit #1

---
+++ Assets/Scripts/Controllers/GameController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace Controllers
+{
+    public class GameController : MonoBehaviour
+    {
+
+        void Awake()
+        {
+            if (_instance != null && _instance != this)
+                Destroy(gameObject);
+            else
+                _instance = this;
+
+            //Extra start functions/statements.
+        }
+
+        #region Singleton
+        private static GameController _instance;
+        public static GameController Instance
+        {
+            get
+            {
+                if (_instance != null)
+                    return _instance;
+
+                _instance = FindObjectOfType<GameController>();
+                if (_instance != null)
+                    return _instance;
+
+                var obj = new GameObject("ObjectPool");
+                _instance = obj.AddComponent<GameController>();
+                return _instance;
+            }
+        }
+        #endregion
+    }
+}
