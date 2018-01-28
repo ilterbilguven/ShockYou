683dbcaccdde33fc1e09b8cd8bca76aa6edd905b
İlter Bilgüven <ilter@live.com>
2018-01-28 13:48:56 +0300

    various fix

--- Assets/Scripts/Controllers/HandController.cs
+++ Assets/Scripts/Controllers/HandController.cs
@@ -33,4 +33,12 @@ public class HandController : MonoBehaviour {
             Physics2D.IgnoreCollision(GetComponent<Collider2D>(), other.collider);
         }
     }
+
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.collider.CompareTag("Charging") || other.collider.CompareTag("Neutral"))
+        {
+            Physics2D.IgnoreCollision(GetComponent<Collider2D>(), other.collider);
+        }
+    }
 }
